bf4a7184fab7bc2a348ebb4bfc996131bde68bed
Christophe Gouiran <bechris13250@gmail.com>
2024-12-28 21:41:48 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+// A tool to migrate gotest test sourcecode to use testify
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage : %s <dir>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	entries, err := os.ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, entry := range entries {
+		fullPath := filepath.Join(os.Args[1], entry.Name())
+
+		if !strings.HasSuffix(fullPath, "_test.go") {
+			continue
+		}
+
+		fmt.Println("Read", fullPath)
+		fileContent, err := os.ReadFile(fullPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		lines := strings.Split(string(fileContent), "\n")
+
+		newFileContent := modifyFile(lines)
+
+		fmt.Println("Write", fullPath)
+		err = os.WriteFile(fullPath, []byte(strings.Join(newFileContent, "\n")), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func modifyFile(content []string) []string {
+	simpleReplacements := map[string]string{
+		"(c *C)":                                  "()",
+		"c.Assert(err, IsNil)":                    "s.NoError(err)",
+		"c.Assert(err, Not(IsNil))":               "s.Error(err)",
+		"c.Assert(err, Equals, nil)":              "s.NoError(err)",
+		"func Test(t *testing.T) { TestingT(t) }": "",
+		"SetUpSuite(":                             "SetupSuite(",
+		"SetUpTest(":                              "SetupTest(",
+	}
+
+	regexpReplacements := []map[string]string{
+		map[string]string{
+			`Commentf\((.+)\)`:                     `fmt.Sprintf($1)`,
+			`c\.Assert\(err, ErrorMatches, (.+)\)`: `s.ErrorContains(err, $1)`,
+			`c\.Log\((.+)\)`:                       `s.T().Log($1)`,
+		},
+		map[string]string{
+			`c\.Assert\(err, IsNil, (.+)\)`:             `s.NoError(err, $1)`,
+			`c\.Assert\(len\(([^)]+)\), Equals, (.+)\)`: `s.Len($1, $2)`,
+			`c\.Assert\((.+), Equals, (.+), (.+)\)`:     `s.Equal($2, $1, $3)`,
+		},
+		map[string]string{
+			`c\.Assert\(err, Equals, (.+)\)`:        `s.Equal(err, $1)`,
+			`c\.Assert\(err, DeepEquals, (.+)\)`:    `s.Equal(err, $1)`,
+			`c\.Assert\((.+), Equals, true\)`:       `s.True($1)`,
+			`c\.Assert\((.+), Equals, false\)`:      `s.False($1)`,
+			`c\.Assert\((.+), IsNil\)`:              `s.Nil($1)`,
+			`c\.Assert\((.+), Not\(IsNil\)\)`:       `s.NotNil($1)`,
+			`c\.Assert\((.+), Not\(IsNil\), (.+)\)`: `s.NotNil($1, $2)`,
+			`c\.Assert\((.+), NotNil\)`:             `s.NotNil($1)`,
+		},
+		map[string]string{
+			`c\.Assert\((.+), Equals, (.+)\)`:            `s.Equal($2, $1)`,
+			`c\.Assert\((.+), Not\(Equals\), (.+)\)`:     `s.NotEqual($2, $1)`,
+			`c\.Assert\((.+), DeepEquals, (.+)\)`:        `s.Equal($2, $1)`,
+			`c\.Assert\((.+), Not\(DeepEquals\), (.+)\)`: `s.NotEqual($2, $1)`,
+			`c\.Assert\((.+), HasLen, (.+)\)`:            `s.Len($1, $2)`,
+			`c\.Assert\((.+), FitsTypeOf, (.+)\)`:        `s.IsType($2, $1)`,
+		},
+	}
+
+	result := []string{}
+
+	for _, line := range content {
+		for originalString, replacement := range simpleReplacements {
+			line = strings.ReplaceAll(line, originalString, replacement)
+		}
+
+		for _, regexpReplacementMap := range regexpReplacements {
+			for regExpression, replacement := range regexpReplacementMap {
+				rx := regexp.MustCompile(regExpression)
+
+				line = rx.ReplaceAllString(line, replacement)
+			}
+		}
+
+		emptySuiteTypeRe := regexp.MustCompile(`type (.+Suite) struct\{\}`)
+		emptySuiteTypeReplacedLine := emptySuiteTypeRe.ReplaceAllString(line, "type $1 struct {")
+
+		suiteTypeRe := regexp.MustCompile(`type (.+Suite) struct \{`)
+		suiteTypeReplacedLine := suiteTypeRe.ReplaceAllString(line, "type $1 struct {")
+
+		suiteVarRe := regexp.MustCompile(`var _ = Suite\(&(.+Suite)\{\}\)`)
+		suiteVarReplacedLine := suiteVarRe.ReplaceAllString(line, "func Test$1(t *testing.T) {")
+		suiteRunReplacedLine := suiteVarRe.ReplaceAllString(line, "\tsuite.Run(t, new($1))")
+
+		if emptySuiteTypeRe.FindStringSubmatch(line) != nil {
+			result = append(result, emptySuiteTypeReplacedLine)
+			result = append(result, "\tsuite.Suite")
+			result = append(result, "}")
+		} else if suiteTypeRe.FindStringSubmatch(line) != nil {
+			result = append(result, suiteTypeReplacedLine)
+			result = append(result, "\tsuite.Suite")
+		} else if suiteVarRe.FindStringSubmatch(line) != nil {
+			result = append(result, suiteVarReplacedLine)
+			result = append(result, suiteRunReplacedLine)
+			result = append(result, "}")
+		} else {
+			result = append(result, line)
+		}
+	}
+
+	return result
+}
---
+++ main_test.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test(t *testing.T) {
+	for _, testData := range []struct {
+		lines    []string
+		expected []string
+	}{
+		{
+			[]string{"func (s *SubmoduleSuite) TestGitSubmodulesSymlink(c *C) {"},
+			[]string{"func (s *SubmoduleSuite) TestGitSubmodulesSymlink() {"},
+		},
+		{
+			[]string{"c.Assert(err, IsNil)"},
+			[]string{"s.NoError(err)"},
+		},
+		// c.Assert(err, Not(IsNil))
+		{
+			[]string{"c.Assert(err, Not(IsNil))"},
+			[]string{"s.Error(err)"},
+		},
+		{
+			[]string{"c.Assert(err, IsNil, comment)"},
+			[]string{"s.NoError(err, comment)"},
+		},
+		{
+			[]string{"c.Assert(err, Equals, nil)"},
+			[]string{"s.NoError(err)"},
+		},
+		{
+			[]string{"c.Assert(err, Equals, ErrGitModulesSymlink)"},
+			[]string{"s.Equal(err, ErrGitModulesSymlink)"},
+		},
+		{
+			[]string{"c.Assert(str, Equals, expected[i].s)"},
+			[]string{"s.Equal(expected[i].s, str)"},
+		},
+		//
+		{
+			[]string{"c.Assert(idx2, Equals, uint32(idx))"},
+			[]string{"s.Equal(uint32(idx), idx2)"},
+		},
+		//
+		{
+			[]string{"c.Assert(ru.Entries[1].Stages[OurMode], Not(Equals), plumbing.ZeroHash)"},
+			[]string{"s.NotEqual(plumbing.ZeroHash, ru.Entries[1].Stages[OurMode])"},
+		},
+		// c.Assert(b.Hash(), Not(DeepEquals), bb.Hash())
+		{
+			[]string{"c.Assert(b.Hash(), Not(DeepEquals), bb.Hash())"},
+			[]string{"s.NotEqual(bb.Hash(), b.Hash())"},
+		},
+		{
+			[]string{`c.Assert(hash.String()+":"+parent.String(), Equals, hash.String()+":"+commitData.ParentHashes[i].String())`},
+			[]string{`s.Equal(hash.String()+":"+commitData.ParentHashes[i].String(), hash.String()+":"+parent.String())`},
+		},
+		{
+			[]string{"c.Assert(obtained, Equals, test.expected, comment)"},
+			[]string{"s.Equal(test.expected, obtained, comment)"},
+		},
+		//
+		{
+			[]string{"c.Assert(result, DeepEquals, expected)"},
+			[]string{"s.Equal(expected, result)"},
+		},
+		{
+			[]string{"c.Assert(err, DeepEquals, e)"},
+			[]string{"s.Equal(err, e)"},
+		},
+		{
+			[]string{"c.Assert(ok, Equals, true)"},
+			[]string{"s.True(ok)"},
+		},
+		// c.Assert((&Option{Key: "key"}).IsKey("key"), Equals, true)
+		{
+			[]string{`c.Assert((&Option{Key: "key"}).IsKey("key"), Equals, true)`},
+			[]string{`s.True((&Option{Key: "key"}).IsKey("key"))`},
+		},
+		{
+			[]string{"c.Assert(checked, Equals, false)"},
+			[]string{"s.False(checked)"},
+		},
+		{
+			[]string{"c.Assert(obj, IsNil)"},
+			[]string{"s.Nil(obj)"},
+		},
+		// c.Assert(obj, Not(IsNil))
+		{
+			[]string{"c.Assert(obj, Not(IsNil))"},
+			[]string{"s.NotNil(obj)"},
+		},
+		// c.Assert(err, Not(IsNil), comment)
+		{
+			[]string{"c.Assert(obj, Not(IsNil), comment)"},
+			[]string{"s.NotNil(obj, comment)"},
+		},
+		{
+			[]string{"c.Assert(obj, NotNil)"},
+			[]string{"s.NotNil(obj)"},
+		},
+		{
+			[]string{"c.Assert(len(commitData.ParentIndexes), Equals, 0)"},
+			[]string{"s.Len(commitData.ParentIndexes, 0)"},
+		},
+		{
+			[]string{"c.Assert(ps, HasLen, 2)"},
+			[]string{"s.Len(ps, 2)"},
+		},
+		{
+			[]string{`c.Assert(fmt.Sprintf("%x", idx.PackfileChecksum), Equals, f.PackfileHash)`},
+			[]string{`s.Equal(f.PackfileHash, fmt.Sprintf("%x", idx.PackfileChecksum))`},
+		},
+		{
+			[]string{"c.Assert(p, FitsTypeOf, &Parser{})"},
+			[]string{"s.IsType(&Parser{}, p)"},
+		},
+		{
+			[]string{"type ParserSuite struct{}"},
+			[]string{
+				"type ParserSuite struct {",
+				"\tsuite.Suite",
+				"}",
+			},
+		},
+		{
+			[]string{"type ObjectSuite struct {"},
+			[]string{
+				"type ObjectSuite struct {",
+				"\tsuite.Suite",
+			},
+		},
+		{
+			[]string{"var _ = Suite(&ParserSuite{})"},
+			[]string{
+				"func TestParserSuite(t *testing.T) {",
+				"\tsuite.Run(t, new(ParserSuite))",
+				"}",
+			},
+		},
+		{
+			[]string{"func Test(t *testing.T) { TestingT(t) }"},
+			[]string{""},
+		},
+		{
+			[]string{
+				"SetUpSuite(",
+				"SetUpTest(",
+			},
+			[]string{
+				"SetupSuite(",
+				"SetupTest(",
+			},
+		},
+		{
+			[]string{`comment := Commentf("input %d = %v\n", i, test.input)`},
+			[]string{`comment := fmt.Sprintf("input %d = %v\n", i, test.input)`},
+		},
+		{
+			[]string{`c.Assert(err, ErrorMatches, "malformed change.*")`},
+			[]string{`s.ErrorContains(err, "malformed change.*")`},
+		},
+		{
+			[]string{`c.Log("Executing test cases:", tc.description)`},
+			[]string{`s.T().Log("Executing test cases:", tc.description)`},
+		},
+	} {
+		modifiedContent := modifyFile(testData.lines)
+
+		assert.Equal(t, testData.expected, modifiedContent, testData.lines)
+	}
+}
